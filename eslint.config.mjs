64bcc106c7e838c0ed441bbdbc30f2742481d93f
Ryan Luong <lumintriet@gmail.com>
2025-10-06 18:31:40 +0700

    chore(eslint): add Astro plugin and configure parser for .astro files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,13 +3,17 @@
 import eslint from "@eslint/js";
 import { defineConfig, globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
+import eslintPluginAstro from "eslint-plugin-astro";
+
 export default defineConfig(
   globalIgnores([".astro/"]),
   eslint.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
+  ...eslintPluginAstro.configs["jsx-a11y-recommended"],
   {
     languageOptions: {
       parserOptions: {
+        parser: "@typescript-eslint/parser",
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
