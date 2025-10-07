6718a158a0e72e0b5399855ebaca3b57a919ff20
Ryan Luong <lumintriet@gmail.com>
2025-10-07 11:20:14 +0700

    chore(refactor): rename utils.ts to utilities.ts to satisfy ESLint rule

--- src/components/ui/dropdown-menu.tsx
+++ src/components/ui/dropdown-menu.tsx
@@ -2,7 +2,7 @@ import * as DropdownMenuPrimitive from "@radix-ui/react-dropdown-menu";
 import { ChevronRightIcon, CircleIcon, CheckIcon } from "lucide-react";
 import * as React from "react";
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utilities";
 
 function DropdownMenuItem({
   variant = "default",
